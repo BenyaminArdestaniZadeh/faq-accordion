3df30ff17d562a9c92225b75a1d59e93160686da
benyamin ardestani zadeh <benyaminardestanizadeh@outlook.com>
2024-01-03 18:47:35 +0330

    feat: define data for items

---
+++ src/components/data.ts
@@ -0,0 +1,17 @@
+export const ItemsData = [
+  {
+    question: "What is React js?",
+    answer:
+      "React is a free and open-source front-end JavaScript library for building user interfaces based on components. It is maintained by Meta and a community of individual developers and companies. React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js.",
+  },
+  {
+    question: "Why React?",
+    answer:
+      "In summary, React is a popular choice for building web applications due to its flexibility, ease of use, and high performance. Its widely used by companies around the world and has a large and active community, which means there are plenty of resources available for developers to learn from.",
+  },
+  {
+    question: "What is the most important topics in react js?",
+    answer:
+      "HTML + CSS , JSX , JavaScript Fundamentals + ES6 , Git , Node + npm,  Node + npm",
+  },
+];
